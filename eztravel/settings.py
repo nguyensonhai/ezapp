96f5993ef74e0c9eee1272f259efab7aa41f35dd
Hải Sơn Nguyễn <nguyensonhai1009@gmail.com>
2019-10-03 21:19:48 +0700

    update

--- eztravel/settings.py
+++ eztravel/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'eztravel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', 
+#        'NAME': 'eztravel',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'eztravel',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
